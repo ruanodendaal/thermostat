0a5641d4c089b7f9d925285bc7c50533552f91ac
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-12 15:57:53 +0100

    connect to interface

---
+++ js/thermostat.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $( "#temperature-up" ).click(function( event ) {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $( "#temperature-down" ).click(function( event ) {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $( "#temperature-reset" ).click(function( event ) {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $( '#powersaving-on' ).hide();
+
+  $( "#powersaving-off" ).click(function( event ) {
+    $( this ).hide();
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('OFF');
+    updateTemperature();
+    $( "#powersaving-on" ).show( );
+  });
+
+  $( "#powersaving-on" ).click(function( event ) {
+    $( this ).hide();
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('ON');
+    updateTemperature();
+    $( "#powersaving-off" ).show( );
+  });
+
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat._temperature);
+  }
+
+
+});
